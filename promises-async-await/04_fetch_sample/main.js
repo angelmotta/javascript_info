05d2cd46e59797c0b7497aa4ff95d33d7c050739
Angel Motta <angel.motta@utec.edu.pe>
2024-05-08 21:53:06 -0500

    refactor: defining independent functions

--- promises-async-await/04_fetch_sample/main.js
+++ promises-async-await/04_fetch_sample/main.js
@@ -1,3 +1,4 @@
+/*
 // fetch return a promise
 fetch("https://javascript.info/article/promise-chaining/user.json")
     .then((response) => response.json())
@@ -23,4 +24,41 @@ fetch("https://javascript.info/article/promise-chaining/user.json")
     )
     .then((githubuser) =>
         alert(`Finished showing ${githubuser.name} info. Bye bro!`)
+    );
+*/
+
+// Refactor
+function getJsonData(url) {
+    return fetch(url).then((response) => response.json()); // response.json() return a Promise
+}
+
+function getGithubUser(username) {
+    return getJsonData(`https://api.github.com/users/${username}`);
+}
+
+function showAvatar(githubuser) {
+    return new Promise(function (resolve, reject) {
+        console.log(githubuser);
+        // modify DOM to show Avatar
+        let img = document.createElement("img");
+        img.src = githubuser.avatar_url;
+        img.className = "promise-avatar-example";
+        document.body.append(img);
+
+        setTimeout(() => {
+            img.remove();
+            console.log(`img.remove() done`);
+            resolve(githubuser);
+        }, 3000);
+    });
+}
+
+// Invoke async functions
+getJsonData("https://javascript.info/article/promise-chaining/user.json")
+    .then((userObj) => getGithubUser(userObj.name))
+    .then((githubUser) => showAvatar(githubUser))
+    .then((githubUser) =>
+        alert(
+            `Finished showing ${githubUser.name} info. Cleaninig all! Bye bro!`
+        )
     );
