394d7847d22a9ff85f6c0ba64ebc23a5e5ee0fe6
Angel Motta <angel.motta@utec.edu.pe>
2024-05-08 21:34:42 -0500

    feat: extending the chain returning a promise

--- promises-async-await/04_fetch_sample/main.js
+++ promises-async-await/04_fetch_sample/main.js
@@ -1,17 +1,26 @@
 // fetch return a promise
 fetch("https://javascript.info/article/promise-chaining/user.json")
     .then((response) => response.json())
-    // after receive userObj data from javascript.info
-    // Make a requesto to Github
+    // after receive userObj data, make a request to Github (return a promise)
     .then((userObj) => fetch(`https://api.github.com/users/${userObj.name}`))
-    .then((response) => response.json())
-    .then((githubuser) => {
-        console.log(githubuser);
-        // modify DOM and show pic profile
-        let img = document.createElement("img");
-        img.src = githubuser.avatar_url;
-        img.className = "promise-avatar-example";
-        document.body.append(img);
+    .then((response) => response.json()) // response.json() return a promise
+    .then(
+        (githubuser) =>
+            new Promise(function (resolve, reject) {
+                console.log(githubuser);
+                // modify DOM and show pic profile
+                let img = document.createElement("img");
+                img.src = githubuser.avatar_url;
+                img.className = "promise-avatar-example";
+                document.body.append(img);
 
-        setTimeout(() => img.remove(), 3000);
-    });
+                setTimeout(() => {
+                    img.remove();
+                    console.log(`img.remove() done`);
+                    resolve(githubuser);
+                }, 3000);
+            })
+    )
+    .then((githubuser) =>
+        alert(`Finished showing ${githubuser.name} info. Bye bro!`)
+    );
